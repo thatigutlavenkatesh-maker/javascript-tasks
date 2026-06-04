fc71caa9cae65f466f985cd87e473769a7067ebf
thatigutlavenkatesh-maker <thatigutlavenkatesh@gmail.com>
2026-06-04 19:36:43 +0530

    Add files via upload

---
+++ task 1.js
@@ -0,0 +1,107 @@
+
+        // Task 1: Variable Practice
+        
+        let studentName = "Venkatesh";
+        let studentAge = 25;
+        let studentCourse = "MERN Stack";
+
+        console.log("Task 1");
+        console.log(studentName);
+        console.log(studentAge);
+        console.log(studentCourse);
+
+
+      
+        // Task 2: Var, Let, Const
+     
+        var company = "Stackly";
+        let salary = 20000;
+        const country = "India";
+
+        company = "Google";
+        salary = 30000;
+
+        console.log("Task 2");
+        console.log("Company: " + company);
+        console.log("Salary: " + salary);
+        console.log("Country: " + country);
+
+
+  
+        // Task 3: User Name
+
+        let userName = prompt("What is your name?");
+
+        console.log("Task 3");
+        console.log("Welcome " + userName);
+
+
+        // Task 4: Confirm Box
+     
+        let jsKnowledge = confirm("Do you know JavaScript?");
+
+        console.log("Task 4");
+        console.log(jsKnowledge);
+
+
+        // Task 5: Student Information
+        
+        let stuName = prompt("Enter Student Name:");
+        let stuAge = prompt("Enter Student Age:");
+        let stuCity = prompt("Enter Student City:");
+
+        console.log("Task 5");
+        console.log("Name : " + stuName);
+        console.log("Age : " + stuAge);
+        console.log("City : " + stuCity);
+
+
+        
+        // Task 6: Alert Message
+     
+        let course = "JavaScript";
+
+        alert("Welcome to " + course);
+
+
+  
+        // Task 7: Document Write
+     
+        document.writeln("<h2>Task 7 Output</h2>");
+        document.writeln("Hello Student <br>");
+        document.writeln("Welcome to JavaScript <br><br>");
+
+
+    
+        // Task 8: Favorite Things
+
+        let favoriteFood = "Chicken Biryani";
+        let favoriteColor = "Black";
+        let favoriteMovie = "Leo";
+
+        console.log("Task 8");
+        console.log("Food : " + favoriteFood);
+        console.log("Color : " + favoriteColor);
+        console.log("Movie : " + favoriteMovie);
+
+
+        // Task 9: Mini Bio Data
+     
+        let name = "Venkatesh";
+        let age = 25;
+        let phone = "9876543210";
+        let email = "venkatesh@gmail.com";
+
+        console.log("Task 9");
+        console.log("Name : " + name);
+        console.log("Age : " + age);
+        console.log("Phone : " + phone);
+        console.log("Email : " + email);
+
+
+        // Task 10: Challenge
+   
+        let user = prompt("Enter Your Name:");
+        let userCourse = prompt("Enter Your Course:");
+
+        alert("Hello " + user + ", Welcome to " + userCourse + " Course");
